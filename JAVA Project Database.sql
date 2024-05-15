ef103394797271c1e3a0e901863640984dbf6c97
ve6er <117016043+ve6er@users.noreply.github.com>
2024-05-15 09:41:19 +0530

    Update JAVA Project Database.sql

--- JAVA Project Database.sql
+++ JAVA Project Database.sql
@@ -1,4 +1,3 @@
-show databases;
 CREATE DATABASE fitness_app;
 
 USE fitness_app;
@@ -13,16 +12,15 @@ CREATE TABLE users (
     age INT,
     INDEX (username)  -- Add an index on the username column
 );
-drop table posts;
+
 CREATE TABLE posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255),
     post_date DATETIME,
-    post_image MEDIUMBLOB,
-    likes INT DEFAULT 0,
+    post_image MEDIUMBLOB
     FOREIGN KEY (username) REFERENCES users(username)
 );
-drop table comments;
+
 CREATE TABLE comments (
     id INT AUTO_INCREMENT PRIMARY KEY,
     post_id INT,
@@ -30,10 +28,11 @@ CREATE TABLE comments (
     comment TEXT,
     comment_date DATETIME,
     likes INT,
+    is_positive BIT DEFAULT FALSE,
     FOREIGN KEY (post_id) REFERENCES posts(id),
     FOREIGN KEY (username) REFERENCES users(username)
 );
-drop table likes;
+
 CREATE TABLE likes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     post_id INT,
@@ -41,9 +40,6 @@ CREATE TABLE likes (
     FOREIGN KEY (post_id) REFERENCES posts(id),
     FOREIGN KEY (username) REFERENCES users(username)
 );
-
-drop table user_details;
-drop table users;
 
 CREATE TABLE user_details (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -55,28 +51,3 @@ CREATE TABLE user_details (
     profile_photo MEDIUMBLOB,
     FOREIGN KEY (id) REFERENCES users(id)
 );
-drop table posts;
-drop table users;
-drop table user_details;
-drop table comments;
-drop table likes;
-
-
-
-
-
-ALTER TABLE comments
-ADD COLUMN is_positive BIT DEFAULT FALSE;
-
-ALTER TABLE posts
-DROP COLUMN likes;
-
-select * from users;	  
-select * from user_details;
-select * from posts;
-select * from comments;
-select * from likes;
-SELECT LENGTH(post_image) AS photo_size FROM posts WHERE id = 7;
-SELECT LENGTH(profile_photo) AS photo_size FROM user_details;
-SELECT u.username, ud.bio, ud.profile_photo FROM users u JOIN user_details ud ON u.id = ud.id WHERE u.username = "verr";
-DELETE from posts WHERE id=1; 
